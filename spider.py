15192f26e164383df31787a5e29e2c4437475403
ShaneRepo <shane8641@live.com>
2017-05-12 13:17:57 -0400

    Added a function to gather links and parse them.

--- spider.py
+++ spider.py
@@ -31,4 +31,26 @@ class Spider:
         Spider.crawled = file_to_set(Spider.crawled_file)
 
     @staticmethod
-    def crawl_page():
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            responce = urlopen(page_url)
+            if responce.getheader('Content-Type') == 'text/html':
+                html_bytes = responce.read()
+                html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+        except:
+            print('Error: cannot crawl page')
+            return set()
+        return finder.page_links()
