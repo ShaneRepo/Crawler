1ed5e38aa4210af8c3425cd3afd4752510bee252
ShaneRepo <shane8641@live.com>
2017-05-17 09:03:37 -0400

    Finished Spider class

--- spider.py
+++ spider.py
@@ -57,3 +57,20 @@ class Spider:
 
     @staticmethod
     def add_links_to_queue(links):
+        for url in links:
+            if url in Spider.queue:
+                continue
+            if url in Spider.crawled:
+                continue
+            if Spider.domain_name not in url:
+                continue
+            Spider.queue.add(url)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)
+
+
+
+
